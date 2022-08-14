69a6870c74ad02f025d1253ff64282e952d56def
Om <omparkashladhani@gmail.com>
2022-08-14 14:20:05 +0200

    the form design and bussines logic were written

--- SaleTax/Form1.Designer.cs
+++ SaleTax/Form1.Designer.cs
@@ -1,5 +1,5 @@
 ﻿
-namespace SaleTax
+namespace SalesTaxes
 {
     partial class Form1
     {
@@ -29,13 +29,405 @@ namespace SaleTax
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.FoodTextBox = new System.Windows.Forms.TextBox();
+            this.BooksTextBox = new System.Windows.Forms.TextBox();
+            this.MedialTextBox = new System.Windows.Forms.TextBox();
+            this.SomethingtextBox = new System.Windows.Forms.TextBox();
+            this.Buy = new System.Windows.Forms.Button();
+            this.label7 = new System.Windows.Forms.Label();
+            this.FoodPricetextBox = new System.Windows.Forms.TextBox();
+            this.BooksPricetextBox = new System.Windows.Forms.TextBox();
+            this.MedicalPricetextBox = new System.Windows.Forms.TextBox();
+            this.SomethingPricetextBox = new System.Windows.Forms.TextBox();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.label10 = new System.Windows.Forms.Label();
+            this.label11 = new System.Windows.Forms.Label();
+            this.label12 = new System.Windows.Forms.Label();
+            this.NIFPtextBox = new System.Windows.Forms.TextBox();
+            this.NIBPtextBox = new System.Windows.Forms.TextBox();
+            this.NIMPtextBox = new System.Windows.Forms.TextBox();
+            this.IMSPtextBox = new System.Windows.Forms.TextBox();
+            this.NIFQtextBox = new System.Windows.Forms.TextBox();
+            this.NIBQtextBox = new System.Windows.Forms.TextBox();
+            this.NIMQtextBox = new System.Windows.Forms.TextBox();
+            this.NISQtextBox = new System.Windows.Forms.TextBox();
+            this.label13 = new System.Windows.Forms.Label();
+            this.label14 = new System.Windows.Forms.Label();
+            this.label15 = new System.Windows.Forms.Label();
+            this.label16 = new System.Windows.Forms.Label();
+            this.textBox9 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(72, 184);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(82, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Imported Items :";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(677, 184);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(105, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Non-Imported Items :";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(33, 220);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(37, 13);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Food :";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(27, 275);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(43, 13);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Books :";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(24, 337);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(50, 13);
+            this.label5.TabIndex = 4;
+            this.label5.Text = "Medical :";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(24, 392);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(86, 13);
+            this.label6.TabIndex = 5;
+            this.label6.Text = "Something Else :";
+            // 
+            // FoodTextBox
+            // 
+            this.FoodTextBox.Location = new System.Drawing.Point(137, 220);
+            this.FoodTextBox.Name = "FoodTextBox";
+            this.FoodTextBox.Size = new System.Drawing.Size(100, 20);
+            this.FoodTextBox.TabIndex = 6;
+            // 
+            // BooksTextBox
+            // 
+            this.BooksTextBox.Location = new System.Drawing.Point(137, 275);
+            this.BooksTextBox.Name = "BooksTextBox";
+            this.BooksTextBox.Size = new System.Drawing.Size(100, 20);
+            this.BooksTextBox.TabIndex = 7;
+            // 
+            // MedialTextBox
+            // 
+            this.MedialTextBox.Location = new System.Drawing.Point(137, 340);
+            this.MedialTextBox.Name = "MedialTextBox";
+            this.MedialTextBox.Size = new System.Drawing.Size(100, 20);
+            this.MedialTextBox.TabIndex = 8;
+            // 
+            // SomethingtextBox
+            // 
+            this.SomethingtextBox.Location = new System.Drawing.Point(137, 392);
+            this.SomethingtextBox.Name = "SomethingtextBox";
+            this.SomethingtextBox.Size = new System.Drawing.Size(100, 20);
+            this.SomethingtextBox.TabIndex = 9;
+            // 
+            // Buy
+            // 
+            this.Buy.Location = new System.Drawing.Point(371, 461);
+            this.Buy.Name = "Buy";
+            this.Buy.Size = new System.Drawing.Size(75, 23);
+            this.Buy.TabIndex = 10;
+            this.Buy.Text = "Buy";
+            this.Buy.UseVisualStyleBackColor = true;
+            this.Buy.Click += new System.EventHandler(this.Buy_Click);
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(12, 41);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(86, 13);
+            this.label7.TabIndex = 11;
+            this.label7.Text = "Imported Prices :";
+            // 
+            // FoodPricetextBox
+            // 
+            this.FoodPricetextBox.Location = new System.Drawing.Point(126, 41);
+            this.FoodPricetextBox.Name = "FoodPricetextBox";
+            this.FoodPricetextBox.Size = new System.Drawing.Size(100, 20);
+            this.FoodPricetextBox.TabIndex = 12;
+            // 
+            // BooksPricetextBox
+            // 
+            this.BooksPricetextBox.Location = new System.Drawing.Point(283, 41);
+            this.BooksPricetextBox.Name = "BooksPricetextBox";
+            this.BooksPricetextBox.Size = new System.Drawing.Size(100, 20);
+            this.BooksPricetextBox.TabIndex = 13;
+            // 
+            // MedicalPricetextBox
+            // 
+            this.MedicalPricetextBox.Location = new System.Drawing.Point(470, 38);
+            this.MedicalPricetextBox.Name = "MedicalPricetextBox";
+            this.MedicalPricetextBox.Size = new System.Drawing.Size(100, 20);
+            this.MedicalPricetextBox.TabIndex = 14;
+            // 
+            // SomethingPricetextBox
+            // 
+            this.SomethingPricetextBox.Location = new System.Drawing.Point(641, 38);
+            this.SomethingPricetextBox.Name = "SomethingPricetextBox";
+            this.SomethingPricetextBox.Size = new System.Drawing.Size(100, 20);
+            this.SomethingPricetextBox.TabIndex = 15;
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(144, 13);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(31, 13);
+            this.label8.TabIndex = 16;
+            this.label8.Text = "Food";
+            this.label8.Click += new System.EventHandler(this.label8_Click);
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(304, 13);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(37, 13);
+            this.label9.TabIndex = 17;
+            this.label9.Text = "Books";
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Location = new System.Drawing.Point(496, 12);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(44, 13);
+            this.label10.TabIndex = 18;
+            this.label10.Text = "Medical";
+            // 
+            // label11
+            // 
+            this.label11.AutoSize = true;
+            this.label11.Location = new System.Drawing.Point(650, 9);
+            this.label11.Name = "label11";
+            this.label11.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.label11.Size = new System.Drawing.Size(80, 13);
+            this.label11.TabIndex = 19;
+            this.label11.Text = "Something Else";
+            // 
+            // label12
+            // 
+            this.label12.AutoSize = true;
+            this.label12.Location = new System.Drawing.Point(12, 103);
+            this.label12.Name = "label12";
+            this.label12.Size = new System.Drawing.Size(104, 13);
+            this.label12.TabIndex = 20;
+            this.label12.Text = "Non imported prices:";
+            // 
+            // NIFPtextBox
+            // 
+            this.NIFPtextBox.Location = new System.Drawing.Point(126, 100);
+            this.NIFPtextBox.Name = "NIFPtextBox";
+            this.NIFPtextBox.Size = new System.Drawing.Size(100, 20);
+            this.NIFPtextBox.TabIndex = 21;
+            this.NIFPtextBox.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // NIBPtextBox
+            // 
+            this.NIBPtextBox.Location = new System.Drawing.Point(283, 100);
+            this.NIBPtextBox.Name = "NIBPtextBox";
+            this.NIBPtextBox.Size = new System.Drawing.Size(100, 20);
+            this.NIBPtextBox.TabIndex = 22;
+            // 
+            // NIMPtextBox
+            // 
+            this.NIMPtextBox.Location = new System.Drawing.Point(470, 100);
+            this.NIMPtextBox.Name = "NIMPtextBox";
+            this.NIMPtextBox.Size = new System.Drawing.Size(100, 20);
+            this.NIMPtextBox.TabIndex = 23;
+            // 
+            // IMSPtextBox
+            // 
+            this.IMSPtextBox.Location = new System.Drawing.Point(641, 100);
+            this.IMSPtextBox.Name = "IMSPtextBox";
+            this.IMSPtextBox.Size = new System.Drawing.Size(100, 20);
+            this.IMSPtextBox.TabIndex = 24;
+            // 
+            // NIFQtextBox
+            // 
+            this.NIFQtextBox.Location = new System.Drawing.Point(678, 217);
+            this.NIFQtextBox.Name = "NIFQtextBox";
+            this.NIFQtextBox.Size = new System.Drawing.Size(100, 20);
+            this.NIFQtextBox.TabIndex = 25;
+            // 
+            // NIBQtextBox
+            // 
+            this.NIBQtextBox.Location = new System.Drawing.Point(680, 272);
+            this.NIBQtextBox.Name = "NIBQtextBox";
+            this.NIBQtextBox.Size = new System.Drawing.Size(100, 20);
+            this.NIBQtextBox.TabIndex = 26;
+            // 
+            // NIMQtextBox
+            // 
+            this.NIMQtextBox.Location = new System.Drawing.Point(680, 337);
+            this.NIMQtextBox.Name = "NIMQtextBox";
+            this.NIMQtextBox.Size = new System.Drawing.Size(100, 20);
+            this.NIMQtextBox.TabIndex = 27;
+            // 
+            // NISQtextBox
+            // 
+            this.NISQtextBox.Location = new System.Drawing.Point(680, 389);
+            this.NISQtextBox.Name = "NISQtextBox";
+            this.NISQtextBox.Size = new System.Drawing.Size(100, 20);
+            this.NISQtextBox.TabIndex = 28;
+            // 
+            // label13
+            // 
+            this.label13.AutoSize = true;
+            this.label13.Location = new System.Drawing.Point(536, 396);
+            this.label13.Name = "label13";
+            this.label13.Size = new System.Drawing.Size(86, 13);
+            this.label13.TabIndex = 32;
+            this.label13.Text = "Something Else :";
+            // 
+            // label14
+            // 
+            this.label14.AutoSize = true;
+            this.label14.Location = new System.Drawing.Point(536, 341);
+            this.label14.Name = "label14";
+            this.label14.Size = new System.Drawing.Size(50, 13);
+            this.label14.TabIndex = 31;
+            this.label14.Text = "Medical :";
+            // 
+            // label15
+            // 
+            this.label15.AutoSize = true;
+            this.label15.Location = new System.Drawing.Point(539, 279);
+            this.label15.Name = "label15";
+            this.label15.Size = new System.Drawing.Size(43, 13);
+            this.label15.TabIndex = 30;
+            this.label15.Text = "Books :";
+            // 
+            // label16
+            // 
+            this.label16.AutoSize = true;
+            this.label16.Location = new System.Drawing.Point(545, 224);
+            this.label16.Name = "label16";
+            this.label16.Size = new System.Drawing.Size(37, 13);
+            this.label16.TabIndex = 29;
+            this.label16.Text = "Food :";
+            // 
+            // textBox9
+            // 
+            this.textBox9.BackColor = System.Drawing.SystemColors.HotTrack;
+            this.textBox9.ForeColor = System.Drawing.Color.Red;
+            this.textBox9.Location = new System.Drawing.Point(6, 146);
+            this.textBox9.Multiline = true;
+            this.textBox9.Name = "textBox9";
+            this.textBox9.Size = new System.Drawing.Size(953, 10);
+            this.textBox9.TabIndex = 33;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.BackColor = System.Drawing.SystemColors.GradientInactiveCaption;
+            this.ClientSize = new System.Drawing.Size(971, 525);
+            this.Controls.Add(this.textBox9);
+            this.Controls.Add(this.label13);
+            this.Controls.Add(this.label14);
+            this.Controls.Add(this.label15);
+            this.Controls.Add(this.label16);
+            this.Controls.Add(this.NISQtextBox);
+            this.Controls.Add(this.NIMQtextBox);
+            this.Controls.Add(this.NIBQtextBox);
+            this.Controls.Add(this.NIFQtextBox);
+            this.Controls.Add(this.IMSPtextBox);
+            this.Controls.Add(this.NIMPtextBox);
+            this.Controls.Add(this.NIBPtextBox);
+            this.Controls.Add(this.NIFPtextBox);
+            this.Controls.Add(this.label12);
+            this.Controls.Add(this.label11);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.SomethingPricetextBox);
+            this.Controls.Add(this.MedicalPricetextBox);
+            this.Controls.Add(this.BooksPricetextBox);
+            this.Controls.Add(this.FoodPricetextBox);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.Buy);
+            this.Controls.Add(this.SomethingtextBox);
+            this.Controls.Add(this.MedialTextBox);
+            this.Controls.Add(this.BooksTextBox);
+            this.Controls.Add(this.FoodTextBox);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Shopping Form";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox FoodTextBox;
+        private System.Windows.Forms.TextBox BooksTextBox;
+        private System.Windows.Forms.TextBox MedialTextBox;
+        private System.Windows.Forms.TextBox SomethingtextBox;
+        private System.Windows.Forms.Button Buy;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.TextBox FoodPricetextBox;
+        private System.Windows.Forms.TextBox BooksPricetextBox;
+        private System.Windows.Forms.TextBox MedicalPricetextBox;
+        private System.Windows.Forms.TextBox SomethingPricetextBox;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.Label label11;
+        private System.Windows.Forms.Label label12;
+        private System.Windows.Forms.TextBox NIFPtextBox;
+        private System.Windows.Forms.TextBox NIBPtextBox;
+        private System.Windows.Forms.TextBox NIMPtextBox;
+        private System.Windows.Forms.TextBox IMSPtextBox;
+        private System.Windows.Forms.TextBox NIFQtextBox;
+        private System.Windows.Forms.TextBox NIBQtextBox;
+        private System.Windows.Forms.TextBox NIMQtextBox;
+        private System.Windows.Forms.TextBox NISQtextBox;
+        private System.Windows.Forms.Label label13;
+        private System.Windows.Forms.Label label14;
+        private System.Windows.Forms.Label label15;
+        private System.Windows.Forms.Label label16;
+        private System.Windows.Forms.TextBox textBox9;
     }
 }
 
--- SaleTax/Form1.cs
+++ SaleTax/Form1.cs
@@ -8,13 +8,125 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
-namespace SaleTax
+namespace SalesTaxes
 {
     public partial class Form1 : Form
     {
+
+        public void setDefaultValues()
+        {
+            FoodPricetextBox.Text = "0";
+            BooksPricetextBox.Text = "0";
+            MedicalPricetextBox.Text = "0";
+            SomethingPricetextBox.Text = "0";
+            FoodTextBox.Text = "0";
+            BooksTextBox.Text = "0";
+            MedialTextBox.Text = "0";
+            NIFPtextBox.Text = "0";
+            NIBPtextBox.Text = "0";
+            NIMPtextBox.Text = "0";
+            NISQtextBox.Text = "0";
+            IMSPtextBox.Text = "0";
+            NIFQtextBox.Text = "0";
+            NIBQtextBox.Text = "0";
+            NIMQtextBox.Text = "0";
+            SomethingtextBox.Text = "0";
+        }
         public Form1()
         {
             InitializeComponent();
+            setDefaultValues();
+        }
+        private void label8_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Buy_Click(object sender, EventArgs e)
+        {
+            List<double> totals = ImportedTaxes();
+            MessageBox.Show("the total tax is :" + totals[0] + "\n" +
+                "the total is :" + totals[1]);
+        }
+
+        public List<double> ImportedTaxes()
+        {
+            // getting prices
+            double foodPrice = double.Parse(FoodPricetextBox.Text);
+            double booksPrice = double.Parse(BooksPricetextBox.Text);
+            double medicalPrice = double.Parse(MedicalPricetextBox.Text);
+            double somethingPrice = double.Parse(SomethingPricetextBox.Text);
+
+            // non imported prices
+            double nonImportedFoodPrice = double.Parse(NIFPtextBox.Text);
+            double nonImportedBooksPrice = double.Parse(NIBPtextBox.Text);
+            double noniImportedMedicalPrice = double.Parse(NIMPtextBox.Text);
+            double nonImportedOtherPrice = double.Parse(IMSPtextBox.Text);
+
+            // getting quantities
+            double foodQ = double.Parse(FoodTextBox.Text);
+            double booksQ = double.Parse(BooksTextBox.Text);
+            double medicalQ = double.Parse(MedialTextBox.Text);
+            double somethingQ= double.Parse(SomethingtextBox.Text);
+
+            // non imported quantities
+            double NonImportedFoodQuantity = double.Parse(NIFQtextBox.Text);
+            double NonImportedBooksQuantity = double.Parse(NIBQtextBox.Text);
+            double NonImportedMedicalQuantity = double.Parse(NIMQtextBox.Text);
+            double NonImportedOtherQuantity = double.Parse(NISQtextBox.Text);
+
+            // calculating non imported tax percentage
+            double NonImportFoodBill = NonImportedFoodQuantity * nonImportedFoodPrice;
+            double NonImportBooksBill = NonImportedBooksQuantity * nonImportedBooksPrice;
+            double NonImportMedicalBill = NonImportedMedicalQuantity * noniImportedMedicalPrice;
+
+            double NonImportOtherBill = NonImportedOtherQuantity * nonImportedOtherPrice;
+            double NonImportedTaxOther = 10 * NonImportOtherBill / 100;
+            double NonImportedHasTobePaid = NonImportedTaxOther + NonImportOtherBill;
+
+
+
+            // calculating the percentages
+            double foodGrossPrice = foodPrice * foodQ;
+            double foodTax = 5 * foodGrossPrice / 100;
+            double lastPriceFood = foodTax + foodPrice;
+
+            double booksGrossPrice = booksPrice * booksQ;
+            double bookTax = 5 * booksGrossPrice / 100;
+            double bookPriceWithTax = bookTax + booksGrossPrice;
+
+            double medicalGrossPrice = medicalPrice * medicalQ;
+            double medicalTax = 5 * medicalGrossPrice / 100;
+            double medicalPriceWithTax = medicalTax + medicalGrossPrice;
+
+            double somethingGrossPrice = somethingPrice * somethingQ;
+            double somethingTax = 15 * somethingGrossPrice / 100;
+            double priceTobePaidSome = somethingTax + somethingGrossPrice;
+
+            double totalTax = somethingTax + bookTax +
+                medicalTax + foodTax +
+                + NonImportedTaxOther;
+
+            double total = lastPriceFood + bookPriceWithTax + 
+                medicalPriceWithTax + priceTobePaidSome +
+                NonImportFoodBill + NonImportBooksBill + 
+                NonImportMedicalBill + NonImportedHasTobePaid
+                ;
+            List<double> totals = new List<double>();
+            totals.Add(totalTax);
+            totals.Add(total);
+            return totals;
+
         }
     }
 }
